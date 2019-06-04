8b309dd6bd39fdf073103b18507f6ff9bc92768e
Sidharath <sid.aggarwal.31@gmail.com>
2019-06-04 02:41:32 -0400

    Added data package that contains the database

--- app/src/main/java/com/example/owner/mypetsapp/EditorActivity.java
+++ app/src/main/java/com/example/owner/mypetsapp/EditorActivity.java
@@ -12,6 +12,8 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.example.owner.mypetsapp.data.PetContract.PetEntry;
+
 public class EditorActivity extends AppCompatActivity {
 
     /** EditText field to enter the pet's name */
@@ -39,7 +41,7 @@ public class EditorActivity extends AppCompatActivity {
         mWeightEditText = (EditText) findViewById(R.id.edit_pet_weight);
         mGenderSpinner = (Spinner) findViewById(R.id.spinner_gender);
 
-        
+
         setupSpinner();
     }
 
@@ -65,11 +67,11 @@ public class EditorActivity extends AppCompatActivity {
                 String selection = (String) parent.getItemAtPosition(position);
                 if (!TextUtils.isEmpty(selection)) {
                     if (selection.equals(getString(R.string.gender_male))) {
-                        mGender = 1; // Male
+                        mGender = PetEntry.GENDER_MALE; // Male
                     } else if (selection.equals(getString(R.string.gender_female))) {
-                        mGender = 2; // Female
+                        mGender = PetEntry.GENDER_FEMALE; // Female
                     } else {
-                        mGender = 0; // Unknown
+                        mGender = PetEntry.GENDER_UNKNOWN; // Unknown
                     }
                 }
             }
